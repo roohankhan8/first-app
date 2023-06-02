1515217e0b3005bd9bc1e991caebd68eea62236c
Roohan Khan <roohank8@gmail.com>
2023-06-02 15:39:55 -0700

    first

---
+++ src/app/housing.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+  url = 'http://localhost:3000/locations';
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+  
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+  constructor() { }
+}
