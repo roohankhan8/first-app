d4c1413f365c6fa1ecf59261b794df805e0a4395
Roohan Khan <roohank8@gmail.com>
2023-06-03 13:06:49 -0700

    First

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -6,12 +6,14 @@ import { HousingLocation } from './housinglocation';
 })
 export class HousingService {
   url = 'http://localhost:3000/locations';
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
   }
   
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
